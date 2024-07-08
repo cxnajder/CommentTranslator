9340fe1339cc25f4746efad3518c1575d6b32b8f
cxnajder <259448@student.pwr.edu.pl>
2024-07-08 22:12:13 +0200

    Add function to find inline comments

---
+++ performFuncOnComment.py
@@ -0,0 +1,81 @@
+import os
+
+_extensionsCommentsMapInline = {
+    'py': '#',
+    'cpp': '//',
+    'h': '//',
+    'c': '//',
+    'cs': '//',
+    'js': '//',
+    'pas': '//',
+}
+
+def _LogError(e):
+    print(f"Error: {e}")
+
+def _getFirstFileExtension(fileName):
+    '''
+    Expectation:
+    "main.py" => "py"
+    "config.h.in" => "h"
+    "text" => ""
+    '''
+    parts = fileName.split('.')
+    if len(parts) > 1:
+        return parts[1]
+    else:
+        return ''
+
+def __testGetFirstFileExtension(fileName):
+    return f"{fileName} => {_getFirstFileExtension(fileName)}"
+
+def runTestsGetFirstFileExtension():
+    print(subtitleDecorator("Testing _getFirstFileExtension(fileName):"))
+    print(testDecoraotr(__testGetFirstFileExtension("main.py"))) # py
+    print(testDecoraotr(__testGetFirstFileExtension("config.h.in"))) # h
+    print(testDecoraotr(__testGetFirstFileExtension("text"))) # (empty)
+
+def testDecoraotr(str):
+    return f"> {str}"
+
+def subtitleDecorator(str):
+    return f"| {str}"
+
+def performFuncOnCommentInline(folderPath, func, extensionsCommentsMap):
+    for root, _, files in os.walk(folderPath):
+        for fileName in files:
+            fileExtension = _getFirstFileExtension(fileName)
+            if fileExtension not in extensionsCommentsMap:
+                _LogError(f"File extension not in extensions map: {fileName} in {root}")
+                continue
+            commentIndicator = extensionsCommentsMap[fileExtension]
+            filePath = os.path.join(root, fileName)
+            with open(filePath, 'r', encoding='utf-8') as file:
+                for lineNumber, line in enumerate(file, start=1):
+                    strippedLine = line.strip()
+                    index = strippedLine.find(commentIndicator)
+                    if index == -1:
+                        continue
+                    commentContent = strippedLine[index + len(commentIndicator):].strip()
+                    func(commentContent, filePath, lineNumber)
+
+
+def __tetsFuncForPerformFuncOnCommentInline(commentContent, filePath, lineNumber):
+    return f"{filePath}#L{lineNumber}:{commentContent};"
+
+def _tetsFuncPrintForPerformFuncOnCommentInline(commentContent, filePath, lineNumber):
+    print(testDecoraotr(__tetsFuncForPerformFuncOnCommentInline(commentContent, filePath, lineNumber)))
+
+def runTestPerformFuncOnCommentInline():
+    samplesFolderPath = ".\samples"
+    testedFunc = _tetsFuncPrintForPerformFuncOnCommentInline
+
+    print(subtitleDecorator("Testing performFuncOnCommentInline(folderPath, func, extensionsCommentsMap):"))
+    performFuncOnCommentInline(samplesFolderPath, testedFunc, _extensionsCommentsMapInline)
+
+if __name__ == "__main__":
+    # Some Tests
+    print("Script run as __main__")
+    print("Starting tests:")
+    runTestsGetFirstFileExtension()
+    runTestPerformFuncOnCommentInline()
