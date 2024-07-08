5f2e2a906a463ca7c095e85b59ff2e5121a1bfb7
cxnajder <259448@student.pwr.edu.pl>
2024-07-08 20:59:20 +0200

    Add block comment to py sample

--- samples/py/sample.py
+++ samples/py/sample.py
@@ -2,5 +2,9 @@ def Main():
     print("Hello, World!")
 
 if __name__ == "__main__":
+    '''
+    This is python block comment:
+    नमस्कार
+    '''
     Main()
     # 朋友们，罗马人民啊，借我你们的耳朵；我来不是为了赞美凯撒，而是为了埋葬他。
