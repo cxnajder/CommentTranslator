5fc74b0374383694ec1b1c251ba2a41f5cec87b4
cxnajder <259448@student.pwr.edu.pl>
2024-07-08 22:11:35 +0200

    Add another comment case to samples

--- samples/c/sample.c
+++ samples/c/sample.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 // This is english comment.
+int x = 5;// comment numero duo.
 int main() {
     printf("Hello, World!\n");
     return 0; // ¿Qué hay en un nombre? Lo que llamamos rosa, con cualquier otro nombre olería igual de dulce.
